c00590333b915569defbb20398f6be88acd43247
ludek <ludek.uhlir@gmail.com>
2017-08-01 18:44:51 +0200

    Add guard-master-noack.c and guard-slave-noalarm.c (one way detection w/o ACK and w/o ALARM)

---
+++ guard-master-noack.c
@@ -0,0 +1,130 @@
+// *********************************************************************
+// *                           E01 - TX                                *
+// *                        RF transmitter                             *
+// *********************************************************************
+// Example of simple RF transmission.
+//
+// Intended for:
+//    HW: IQRF TR modules TR-52B, TR-53B and compatibles
+//        DK-EVAL-03, DK-EVAL-04 development kit
+//    OS: v3.00 or higher
+//
+// Description:
+//    This example shows how to make a simple transmitter. It can be
+//    tested with the E02-RX example.
+//    - RF message is sent (and red LED flashes once) whenever the
+//      pushbutton on the kit is pressed.
+//    - Content of the messages = Application data.
+//      (It is the 32 B array in EEPROM dedicated to user information
+//      about the application - see the IQRF OS manual.
+//      The "01234567890123456789012345678901" data array is used here.
+//    - To lower power consumption the module utilizes the Sleep mode
+//      and Wake-up on pin change for response to the button.
+//      Wake-up on pin change is under user's control
+//      and has to be enabled by the user when needed.
+//    - The watchdog is repeatedly cleared.
+//      If omitted:
+//      - The module is regularly reset from the watchdog overflow
+//        which leads to additional transmission whenever the
+//        watchdog overflows while the button is just pressed.
+//      - Transmission aborted if watchdog overflows while transmitting.
+//
+//
+// File:    E01-TX.c
+// Version: v1.00                                   Revision: 08/12/2010
+//
+// Revision history:
+//     v1.00: 08/12/2010  First release
+//
+// *********************************************************************
+#include "includes/template-basic.h"
+
+// *********************************************************************
+
+#define CTRIGDIR TRISA.0
+#define CTRIG    _C1  
+
+#define SPIDEBUG
+void take_picture(void);
+
+void APPLICATION()             	// Obligatory assigning - see E00-START
+{
+	clearBufferRF();
+	
+	CTRIGDIR = 0; //port direction of portc.3 (sam as _SCK) for camera trigger
+	CTRIG = 0;
+	
+	#ifdef SPIDEBUG
+	enableSPI();	
+	#else //not SPIDEBUG
+	disableSPI();
+	#endif //SPIDEBUG
+	
+	SWDTEN = 1; 				//keep wdt running
+	
+    while (1)                  	// Main cycle (perpetually repeated)
+    {
+        clrwdt();              	// Clear watchdog
+		
+		/*
+		SWDTEN = 0;            	// Minimize consumption (switch off WDT)
+        iqrfSleep();           	// Put the module into Sleep mode --wakeup C5(SS), WDT,poweron/off
+								// Requested sequence for wake-up:
+		pulseLEDR();
+		pulseLEDG();
+		waitDelay(25);
+        */
+		//do not fall asleep, else RX can't work
+		/*
+		GIE = 0;               	//   disable all interrupts
+        RBIE = 1;              	//   enable wake-up on pin change
+
+        SWDTEN = 0;            	// Minimize consumption (switch off WDT)
+        iqrfSleep();           	// Put the module into Sleep mode --wakeup C5(SS), WDT,poweron/off
+        RBIF = 0;              	// Requested: clear flag
+		*/
+		
+		if (RFRXpacket())           // If anything was received		
+        {
+			pulseLEDG();            //   LED indication
+			
+			#ifdef SPIDEBUG
+            copyBufferRF2COM();     //   Copy received RF data  from bufferRF to bufferCOM
+            startSPI(DLEN);         //     and send it via SPI
+			#endif
+			
+			//alarm recevied
+			if (bufferRF[0] == 'A')
+			{				
+				take_picture();
+			}
+        }		
+    }                          	// End of main cycle
+}
+
+void take_picture(void)
+{
+	uns8 i;
+	
+	CTRIG = 1;           // turn on camera and take picture/inform others
+	
+	waitDelay(25);     	//      and wait 250ms (25*10ms)
+	//clrwdt();              	// Clear watchdog
+
+	CTRIG = 0;           // turn off camera
+	pulsingLEDR();      // 4x LED flash (picture taken)
+	waitDelay(95);
+	stopLEDR();
+}
+
+// *********************************************************************
+
+#pragma packedCdataStrings 0    // Store the string unpacked
+                                //   (one byte in one location)
+
+//                             00000000001111111111222222222233
+#pragma cdata[__EEAPPINFO] =  "Commands: 'C'heck status        "
+                                // 32 B to be stored to the
+                                // Application Info array in EEPROM
+// *********************************************************************
+
---
+++ guard-slave-noalarm.c
@@ -0,0 +1,131 @@
+// *********************************************************************
+// *                           E01 - TX                                *
+// *                        RF transmitter                             *
+// *********************************************************************
+// Example of simple RF transmission.
+//
+// Intended for:
+//    HW: IQRF TR modules TR-52B, TR-53B and compatibles
+//        DK-EVAL-03, DK-EVAL-04 development kit
+//    OS: v3.00 or higher
+//
+// Description:
+//    This example shows how to make a simple transmitter. It can be
+//    tested with the E02-RX example.
+//    - RF message is sent (and red LED flashes once) whenever the
+//      pushbutton on the kit is pressed.
+//    - Content of the messages = Application data.
+//      (It is the 32 B array in EEPROM dedicated to user information
+//      about the application - see the IQRF OS manual.
+//      The "01234567890123456789012345678901" data array is used here.
+//    - To lower power consumption the module utilizes the Sleep mode
+//      and Wake-up on pin change for response to the button.
+//      Wake-up on pin change is under user's control
+//      and has to be enabled by the user when needed.
+//    - The watchdog is repeatedly cleared.
+//      If omitted:
+//      - The module is regularly reset from the watchdog overflow
+//        which leads to additional transmission whenever the
+//        watchdog overflows while the button is just pressed.
+//      - Transmission aborted if watchdog overflows while transmitting.
+//
+//
+// File:    E01-TX.c
+// Version: v1.00                                   Revision: 08/12/2010
+//
+// Revision history:
+//     v1.00: 08/12/2010  First release
+//
+// *********************************************************************
+#include "includes/template-basic.h"
+
+// *********************************************************************
+
+#define NUMOFUNIT '0'
+
+//#define SPIDEBUG //it will generate alarms!
+
+void APPLICATION()             	// Obligatory assigning - see E00-START
+{    
+	
+	uns8 motion_detected = 0;
+	uns8 ring_alarm_force_cnt = 0;
+	uns16 wupnum = 0; //number of wakeups (wakeup every 4s by wdt)
+	
+	//set sck as output:
+	#ifndef SPIDEBUG
+	disableSPI();
+	#else
+	enableSPI();                // Enable SPI
+	#endif
+
+    while (1)                  	// Main cycle (perpetually repeated)
+    {
+        clrwdt();              	// Clear watchdog
+		//keep wdt running to wake up every cca 4s
+		SWDTEN = 1;
+		
+		GIE = 0;               	//   disable all interrupts
+		RBIE = 1;              	//   enable wake-up on pin change
+		iqrfSleep();           	// Put the module into Sleep mode
+		RBIF = 0;              	// Requested: clear flag		
+		
+		if (_SS)     	//   PIR sensor activated (PORTA.5, SPI SS (C5))
+		{			
+			motion_detected = 1;			
+			pulseLEDR();			
+			pulseLEDG();
+			waitDelay(25);
+		}else
+		{
+			pulseLEDG();
+			waitDelay(25);
+			wupnum++;
+		}
+		
+		if (motion_detected)
+		{
+			motion_detected	= 0;
+			wupnum = 0; //do not send status packet
+			PIN = 0; //after every RFRXpacket before every RFTXpacket
+			bufferRF[0] = 'A';  //ALARM
+			bufferRF[1] = NUMOFUNIT;   //Number of unit causing alarm
+			bufferRF[2] = '_';
+			bufferRF[3] = getSupplyVoltage()+48;  //2,25V+getSupplyVoltage()*0,1
+			bufferRF[4] = 'V';
+            DLEN = 5;         	//      Setup RF packet length								
+            RFTXpacket();      	//      Transmit the message			
+			pulseLEDR();
+		}
+
+		//15 wakeups per minute, 15*10 => 10min
+		//if (wupnum > 15*10)
+		if (wupnum > 3)
+		{
+			wupnum = 0;
+			bufferRF[0] = 'S';  //STATUS
+			bufferRF[1] = NUMOFUNIT;   //Number of unit causing alarm
+			bufferRF[2] = '_';
+			bufferRF[3] = getSupplyVoltage()+48;  //2,25V+getSupplyVoltage()*0,1
+			bufferRF[4] = 'V';
+            DLEN = 5;         	//      Setup RF packet length
+								//        (only 10 B of Application
+								//        info to be sent)
+            RFTXpacket();      	//      Transmit the message            
+			pulseLEDR();
+			waitDelay(25);     	//      and wait 250ms (25*10ms)
+		}
+    }                          	// End of main cycle
+}
+
+// *********************************************************************
+
+#pragma packedCdataStrings 0    // Store the string unpacked
+                                //   (one byte in one location)
+
+//                             00000000001111111111222222222233
+#pragma cdata[__EEAPPINFO] =  "01234567890123456789012345678901"
+                                // 32 B to be stored to the
+                                // Application Info array in EEPROM
+// *********************************************************************
+
